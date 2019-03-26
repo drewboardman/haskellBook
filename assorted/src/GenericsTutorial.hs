177316f60a6e9ae453333559f21295b43d70a151
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-25 23:58:05 -0500

    testing fugitive

--- assorted/src/GenericsTutorial.hs
+++ assorted/src/GenericsTutorial.hs
@@ -7,5 +7,6 @@ module GenericsTutorial () where
 -- in general you're going to need both an input (f) and an output(g)
 -- parameter. They both need to be of kind * -> * and then be passed this
 -- existentialized x, for dark, unholy reasons known not by humankind.
+
 class GValidate f g where
   gvalidate :: f x -> Maybe (g x)
