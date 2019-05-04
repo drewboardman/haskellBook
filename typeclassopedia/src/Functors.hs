391667ea2b497a71b61f3844d38c90cdbc788064
Drew Boardman <drew.e.boardman@gmail.com>
2019-05-04 13:35:53 -0500

    func

--- typeclassopedia/src/Functors.hs
+++ typeclassopedia/src/Functors.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Functors () where
 
@@ -6,7 +7,7 @@ module Functors () where
 data Either' a b = Left' a | Right' b deriving (Eq, Show, Ord)
 instance Functor (Either' e) where
   fmap func (Right' e) = Right' (func e)
-  fmap _ (Left' e) = Left' e
+  fmap _ (Left' e)     = Left' e
 
 newtype Foo a b = Foo (a -> b)
 instance Functor (Foo a) where
@@ -24,18 +25,34 @@ instance Functor Pair2 where
   fmap :: (a -> b) -> Pair2 a -> Pair2 b
   fmap f (Pair2 x y) = Pair2 (f x) (f y)
 
-    -- Explain their similarities and differences.
--- Because of the type constraint on Pair2, you have to apply the function to both arguments in the pair. For Pair1, that's not even an option.
+{- Explain their similarities and differences.
+
+Because of the type constraint on Pair2, you have to apply the function to both
+arguments in the pair. For Pair1, that's not even an option.
+-}
 
     -- Implement a Functor instance for the type ITree, defined as
 
-    -- data ITree a = Leaf (Int -> a) 
-    --              | Node [ITree a]
+data ITree a = Leaf (Int -> a) | Node [ITree a]
+instance Functor ITree where
+  fmap :: (a -> b) -> ITree a -> ITree b
+  fmap f (Leaf g)                = Leaf (f . g)
+  fmap _ (Node [])               = Node []
+  fmap f (Node [x])              = fmap f x
+  fmap f (Node nonEmpty@(_:_:_)) = Node $ fmap f <$> nonEmpty
 
     -- Give an example of a type of kind * -> * which cannot be made an instance of Functor (without using undefined).
+
+-- data Bar a = Bar { a :: Int }
+-- instance Functor Bar where
+--   fmap :: (a -> b) -> Bar a -> Bar b
+--   fmap f (Bar int) = Bar (f int)
+
     -- Is this statement true or false?
 
     --     The composition of two Functors is also a Functor.
 
     -- If false, give a counterexample; if true, prove it by exhibiting some appropriate Haskell code.
 
+foo :: [Maybe Int] = [Just 1, Nothing, Just 18]
+example = map (\x -> fmap (*2) x) foo
