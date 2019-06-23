78efd2a67b11c2ec40baa120472289d54c25a49c
Drew Boardman <drew.e.boardman@gmail.com>
2019-06-23 17:27:35 -0500

    monads

--- typeclassopedia/src/Monad.hs
+++ typeclassopedia/src/Monad.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE DeriveFoldable      #-}
 {-# LANGUAGE DeriveFunctor       #-}
 {-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Monad () where
 
-newtype ListMonad a = ListMonad [a] deriving (Functor)
+newtype ListMonad a = ListMonad { getListMonad :: [a] } deriving (Functor, Foldable)
 
 instance Applicative ListMonad where
   pure :: a -> ListMonad a
@@ -16,5 +17,21 @@ instance Applicative ListMonad where
 
 instance Monad ListMonad where
   (>>=) :: ListMonad a -> (a -> ListMonad b) -> ListMonad b
-  ListMonad [] >>= _ = ListMonad []
-  ListMonad xs >>= g = join $ [g x | x <- xs]
+  ListMonad xs >>= g = ListMonad (concatMap (getListMonad . g) xs)
+
+-- Function Monad
+
+-- functor instance
+--fmap2 :: (a -> b) -> f a -> f b
+--fmap2 :: (a -> b) -> ((->) e) a) -> ((-> e) b)
+--fmap2 :: (a -> b) -> (e -> a) -> (e -> b)
+
+fmap2 :: (b -> c) -> (a -> b) -> a -> c
+fmap2 f g = f . g
+
+-- newtype for Func ((->) r)
+newtype Func a b = Func { getFunc :: a -> b }
+
+instance Functor (Func a) where
+  fmap :: (b -> c) -> Func a b -> Func a c
+  fmap f (Func a) = Func (f . a)
