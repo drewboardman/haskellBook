1e032dc26f687b494ebd25c175d9120b6321036d
Drew Boardman <drew.e.boardman@gmail.com>
2019-06-26 19:44:13 -0500

    more monads

--- typeclassopedia/src/Monad.hs
+++ typeclassopedia/src/Monad.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Monad () where
+module Monad where
 
 newtype ListMonad a = ListMonad { getListMonad :: [a] } deriving (Functor, Foldable)
 
@@ -35,3 +35,19 @@ newtype Func a b = Func { getFunc :: a -> b }
 instance Functor (Func a) where
   fmap :: (b -> c) -> Func a b -> Func a c
   fmap f (Func a) = Func (f . a)
+
+pure2 :: a -> e -> a
+pure2 x _ = x
+
+-- app2 :: f (a -> b) -> f a -> f b
+app2 :: (e -> a -> b) -> (e -> a) -> e -> b
+f `app2` g = \e -> f e $ g e
+
+pure3 :: b -> Func a b
+pure3 x = Func $ const x
+
+-- monad
+-- bind2 :: f a -> (a -> f b) -> f b
+bind2 :: (e -> a) -> (a -> e -> b) -> e -> b
+-- same as \x -> (flip g x (f x))
+f `bind2` g = \x -> g (f x) x
